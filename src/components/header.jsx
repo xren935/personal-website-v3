f70231e76dce2fe5f710cc8b6f0e4d3628ac29c7
xren935 <59517115+xren935@users.noreply.github.com>
2021-01-10 14:58:39 -0800

    work in progress - header

---
+++ src/components/header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'font-awesome/css/font-awesome.min.css';
+import { Spring } from "react-spring/renderprops";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import '../static/header.css';
+
+class Header extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  render() {
+    Aos.init({interval: 1000});
+    return (
+      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+      {(props) => (
+        <div style={props}>
+            <div className="header">
+                <h1 id="hi">Hi, I'm Annie Ren</h1>
+                <br></br>
+                <div>
+                <h1>
+                    I AM &nbsp;
+                    <div>
+                        <ul>
+                            <li>a Developer</li>
+                            <li>a McGillian</li>
+                            <li>an Engineer-in-training</li>
+                            <li>a Language Lover</li>
+                            <li>a Philomath</li>
+                            <li>a Conscientious Explorer</li>
+                            <li>an Astronomy Enthusiast</li>
+                            <li>a Good Daughter/Friend</li>
+                            <li>a Part-time Foodie</li>
+                            <li>determined</li>
+                        </ul>
+                    </div>
+                </h1>
+                </div>
+            </div>
+        </div>
+      )}
+    </Spring>
+    );
+  }
+}
+
+export default Header;
